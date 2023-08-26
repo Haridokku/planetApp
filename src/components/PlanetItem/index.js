7d687bf90769628d448c1fbfe94b19e77174de61
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-08-26 11:43:33 +0530

    Update index.js

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -1,3 +1,4 @@
+
 import './index.css'
 
 const PlanetItem = props => {
@@ -6,7 +7,6 @@ const PlanetItem = props => {
   return (
     <li>
       <div className="planet-container" data-testid="planets">
-        <h1 className="head">PLANETS</h1>
         <img src={imageUrl} alt={name} className="img" />
         <h1 className="heading">{name}</h1>
         <p className="describe">{description}</p>
