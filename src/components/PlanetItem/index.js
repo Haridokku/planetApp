807717988819aa6a1b76eac21ac9f810dc05bb6a
Haridokku <124425442+Haridokku@users.noreply.github.com>
2023-08-26 15:25:07 +0530

    Update index.js

--- src/components/PlanetItem/index.js
+++ src/components/PlanetItem/index.js
@@ -5,8 +5,8 @@ const PlanetItem = props => {
   const {planetDetails} = props
   const {name, imageUrl, description} = planetDetails
   return (
-    <div className="planet-container" data-testid="planets">
-      <img src={imageUrl} alt={`planet${name}`} className="img" />
+    <div className="planet-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="img" />
       <h1 className="heading">{name}</h1>
       <p className="describe">{description}</p>
     </div>
