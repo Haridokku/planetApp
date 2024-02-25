ee6d15adb96f5bb9b7d690e36f53242095525a88
Haridokku <124425442+Haridokku@users.noreply.github.com>
2024-02-25 16:40:05 +0530

    Update index.js

--- src/components/PlanetsSlider/index.js
+++ src/components/PlanetsSlider/index.js
@@ -6,12 +6,17 @@ import './index.css'
 
 const PlanetsSlider = props => {
   const {planetsList} = props
+  const settings = {
+    slidesToShow: 1,
+    dots:true,
+    slidesToScroll:1
+  }
 
   return (
     <div className="slider-container">
       <div data-testid="planets">
         <h1 className="head">PLANETS</h1>
-        <Slider>
+        <Slider {...settings}>
           {planetsList.map(each => (
             <PlanetItem key={each.id} planetDetails={each} />
           ))}
